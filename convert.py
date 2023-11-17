2b67a4f0c372b57a0218c3386272ef21ff2de252
ioexcept <ioexcept@yahoo.com>
2023-11-17 09:18:09 -0600

    provide the obj name, it will now convert it dynamically

--- convert.py
+++ convert.py
@@ -1,4 +1,20 @@
+import sys
 import aspose.threed as a3d
 
-scene = a3d.Scene.from_file("male_scuba.obj")
-scene.save("male_scuba.stl")
+
+
+def copy_file(filename):
+    try:
+        scene = a3d.Scene.from_file(filename + ".obj")
+        scene.save(filename + ".stl")
+        print("OBJ file converted and saved as " + filename + ".stl")
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <filename>")
+    else:
+        copy_file(sys.argv[1])
